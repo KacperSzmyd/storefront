3faf8795750970617a4c35731e0015331a7ebe13
Kacper <k.g.szmyd@gmail.com>
2024-02-06 13:58:33 +0100

    aggregate func in playground

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from store.models import Product, OrderItem, Order, Collection
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # query_set = Product.objects.values_list("id", "title", "collection__title")
+
+    number_of_orders = Order.objects.aggregate(count=Count("id"))
+    product_1_sold = OrderItem.objects.filter(id=1).aggregate(
+        units_sold=Sum("quantity")
+    )
+    customer_1_orders = Order.objects.filter(customer__id=1).aggregate(
+        count=Count("id")
+    )
+    collection3 = Collection.objects.filter(id=3).aggregate(
+        min=Min("product__unit_price"),
+        max=Max("product__unit_price"),
+        avg=Avg("product__unit_price"),
+    )
+
+    context = {
+        "name": "Kacper",
+        "orders": number_of_orders,
+        "product1": product_1_sold,
+        "customer1": customer_1_orders,
+        "collection": collection3,
+    }
+
+    return render(request, "hello.html", context)

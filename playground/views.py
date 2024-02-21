9763b628b879ce1d4044e89b6312b58c74b27ef3
Kacper <k.g.szmyd@gmail.com>
2024-02-21 15:19:54 +0100

    admin panel customization

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,9 @@
+from django.db.models.aggregates import Count
 from django.shortcuts import render
+from store import models
 
 
 def say_hello(request):
-    context = {"name": "Kacper", "result": "queryset_placeholder"}
+    customers = models.Customer.objects.all().annotate(orders=Count("order__id"))
+    context = {"name": "Kacper", "result": customers}
     return render(request, "hello.html", context)
--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_per_page = 5
+    search_fields = ["label"]

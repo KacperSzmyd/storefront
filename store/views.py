aa34e463766a9fd37996b3aef2eddaea37a920e0
Kacper <k.g.szmyd@gmail.com>
2024-03-05 11:50:40 +0100

    swapping REST views from funvtion based views to class based views

--- store/views.py
+++ store/views.py
@@ -3,6 +3,8 @@ from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
 from rest_framework import status
 
 from .models import Product, Collection
@@ -47,39 +49,42 @@ def collection_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(["GET", "POST"])
-def product_list(request):
-    if request.method == "GET":
+class ProductList(ListCreateAPIView):
+    def get(self, request):
         queryset = Product.objects.select_related("collection").all()
         serializer = ProductSerializer(
             queryset, many=True, context={"request": request}
         )
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == "GET":
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "DELETE":
+
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response(
                 {
                     "error": "Product cannot be deleted due to beeing associatied with an other item"
-                },
-                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                }
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            product.delete
+            return Response(status=status.HTTP_204_NO_CONTENT)

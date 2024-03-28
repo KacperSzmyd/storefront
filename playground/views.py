b92368c0ab6ae1e2975a62f53f49facfd325b3ca
= <=>
2024-03-28 15:48:39 +0100

    automated tests for collections and producs

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,8 @@
-from django.db.models.aggregates import Count
 from django.shortcuts import render
-from store import models
+from .tasks import notify_customers
 
 
 def say_hello(request):
-    customers = models.Customer.objects.all().annotate(orders=Count("order__id"))
-    context = {"name": "Kacper", "result": customers}
+    notify_customers.delay('Hello')
+    context = {"name": "Kacper"}
     return render(request, "hello.html", context)

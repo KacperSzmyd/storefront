359e99c98b351f623b523f181e30d637315fc322
= <=>
2024-04-02 12:33:07 +0200

    playing with low-level cache view

--- locust/browse_products.py
+++ locust/browse_products.py
@@ -7,15 +7,15 @@ class WebsiteUser(HttpUser):
 
     @task(3)
     def view_products(self):
-        collection_id = randint(2, 6)
+        collection_id = randint(3, 6)
         self.client.get(
-            f"/store/products/?collection_id={collection_id}/", name="/store/products"
+            f"/store/products/?collection_id={collection_id}", name="/store/products"
         )
 
     @task(7)
     def view_product(self):
         product_id = randint(1, 1000)
-        self.client.get(f"/store/products/{product_id}/", name="/store/products/:id")
+        self.client.get(f"/store/products/{product_id}", name="/store/products/:id")
 
     @task(1)
     def add_to_cart(self):
@@ -28,6 +28,10 @@ class WebsiteUser(HttpUser):
                 "quantity": product_id + 1,
             },
         )
+
+    @task
+    def playground_hello(self):
+        self.client.get("/playground/hello/")
 
     def on_start(self):
         response = self.client.post(f"/store/carts/")
--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.core.cache import cache
 from .tasks import notify_customers
+import requests
 
 
 def say_hello(request):
-    notify_customers.delay('Hello')
-    context = {"name": "Kacper"}
+    key = "httpbin_result"
+    if cache.get(key) == None:
+        response = requests.get("https://httpbin.org/delay/2")
+        data = response.json()
+        cache.set(key, data)
+
+    context = {
+        "name": cache.get(key),
+    }
     return render(request, "hello.html", context)

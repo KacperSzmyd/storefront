08a2e9a2941546b676a92381260ee7be95e1f41b
= <=>
2024-06-09 13:22:17 +0200

    added redirect to api root

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import include, path
 
+# from django.views.generic import TemplateView
+from .views import go_to_api_root
 
-urlpatterns = [
-    path("", TemplateView.as_view(template_name="core/index.html")),
-]
+urlpatterns = [path("", go_to_api_root), path("store/", include("store.urls"))]

2bb970ebfef747e1f05a9352297f14bac7d2d682
Kacper <k.g.szmyd@gmail.com>
2024-10-04 09:48:08 +0200

    made requirements.txt

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,22 @@
+from django.core.mail import EmailMessage, BadHeaderError
 from django.db.models.aggregates import Count
 from django.shortcuts import render
 from store import models
 
 
 def say_hello(request):
+    try:
+        message = EmailMessage(
+            "dummy subject",
+            "Lorem Ipsum",
+            "kacper@storefront.pl",
+            ["user1@domain.com", "user2@domain.com"],
+        )
+        message.attach_file("playground/static/images/randompic.jpg")
+        message.send()
+    except BadHeaderError:
+        pass
+
     customers = models.Customer.objects.all().annotate(orders=Count("order__id"))
     context = {"name": "Kacper", "result": customers}
     return render(request, "hello.html", context)
